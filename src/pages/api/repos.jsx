9d132eec303bcaa5bc5de91c9da86913cb1a7247
sup! <78882365+slorixsh@users.noreply.github.com>
2022-09-01 17:53:33 +0300

    Update repos.jsx

--- src/pages/api/repos.jsx
+++ src/pages/api/repos.jsx
@@ -1,5 +1,5 @@
-let key1 = 'ghp_A0dqDN637Zf2SSj';
-let key2 = 'oWgciMpgzxZnUWp4PLDF3';
+let key1 = 'ghp_VzcC7gx0GXzOVP1';
+let key2 = '7kIiTmNtBqj2sOC0S0zxD';
 
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/slorixsh/repos', {
@@ -7,3 +7,9 @@ export default async (req, res) => {
             Authorization: 'token '+key1+key2
         }
     })).json()
+    try {
+        res.send([..._, ...__, ...___])
+    } catch {
+        res.status(500);
+    }
+}
