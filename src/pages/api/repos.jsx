26cdfdb7e61e8fa2000337c04a29c4ee2e96fb0c
sup! <78882365+slorixsh@users.noreply.github.com>
2022-09-01 17:29:28 +0300

    Update repos.jsx

--- src/pages/api/repos.jsx
+++ src/pages/api/repos.jsx
@@ -2,7 +2,7 @@ let key1 = 'ghp_A0dqDN637Zf2SSj';
 let key2 = 'oWgciMpgzxZnUWp4PLDF3';
 
 export default async (req, res) => {
-    let _ = await (await fetch('https://api.github.com/users/clqu/repos', {
+    let _ = await (await fetch('https://api.github.com/users/slorixsh/repos', {
         headers: {
             Authorization: 'token '+key1+key2
         }
